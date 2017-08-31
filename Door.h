980b432a841eaf022c87a03d4ce7c6462966c78d
Manuel <troyanlp@gmail.com>
2017-08-31 20:24:47 +0200

    Added images for sprites. Created class Door.

---
+++ Door.h
@@ -0,0 +1,21 @@
+#ifndef __DOOR_H__
+#define __DOOR_H__
+
+#include "Entity.h"
+
+struct SDL_Texture;
+
+class Door :
+	public Entity
+{
+public:
+	Door();
+	~Door();
+
+public:
+	SDL_Texture* graphics = nullptr;
+	SDL_Rect doorSprite;
+	SDL_Rect spriteDest;
+};
+
+#endif
